44eba844f3b4cc33696593236c0481ab15f757aa
eduardalverto-svg <eduardalverto@gmail.com>
2026-04-15 01:26:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ token = os.getenv('TELEGRAM_TOKEN')
 chat_id = os.getenv('TELEGRAM_CHAT_ID')
 
 def enviar_saludo():
-    mensaje = "🚀 ¡Omni-Bot P2P Activado con Éxito! Estoy patrullando el mercado por ti."
+    mensaje = "🚀 ¡Omni-Bot P2P Activado con Éxito! Estoy patrullando."
     url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={mensaje}"
     
     try:
         response = requests.get(url)
         if response.status_code == 200:
-            print("Mensaje enviado correctamente")
+            print("Mensaje enviado")
         else:
-            print(f"Error al enviar: {response.text}")
+            print(f"Error: {response.text}")
     except Exception as e:
-        print(f"Error de red: {e}")
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     enviar_saludo()
